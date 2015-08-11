dfb7724066020d04fd6be781447937281d0767cb
talpachen <talpachen@gmail.com>
2015-08-11 11:49:52 +0800

    CMSIS-DAP_Nu-Link v0.1

--- Firmware/descriptors.c
+++ Firmware/descriptors.c
@@ -8,36 +8,26 @@
 /*!<Includes */
 #include "NUC100Series.h"
 #include "usbd.h"
+#include "usb_hid.h"
 
 uint8_t ReportDescriptor[] =
 {
-    0x05, 0x01,     /* Usage Page(Generic Desktop Controls) */
-    0x09, 0x02,     /* Usage(Mouse) */
-    0xA1, 0x01,     /* Collection(Application) */
-    0x09, 0x01,     /* Usage(Pointer) */
-    0xA1, 0x00,     /* Collection(Physical) */
-    0x05, 0x09,     /* Usage Page(Button) */
-    0x19, 0x01,     /* Usage Minimum(0x1) */
-    0x29, 0x03,     /* Usage Maximum(0x3) */
-    0x15, 0x00,     /* Logical Minimum(0x0) */
-    0x25, 0x01,     /* Logical Maximum(0x1) */
-    0x75, 0x01,     /* Report Size(0x1) */
-    0x95, 0x03,     /* Report Count(0x3) */
-    0x81, 0x02,     /* Input(3 button bit) */
-    0x75, 0x05,     /* Report Size(0x5) */
-    0x95, 0x01,     /* Report Count(0x1) */
-    0x81, 0x01,     /* Input(5 bit padding) */
-    0x05, 0x01,     /* Usage Page(Generic Desktop Controls) */
-    0x09, 0x30,     /* Usage(X) */
-    0x09, 0x31,     /* Usage(Y) */
-    0x09, 0x38,     /* Usage(Wheel) */
-    0x15, 0x81,     /* Logical Minimum(0x81)(-127) */
-    0x25, 0x7F,     /* Logical Maximum(0x7F)(127) */
-    0x75, 0x08,     /* Report Size(0x8) */
-    0x95, 0x03,     /* Report Count(0x3) */
-    0x81, 0x06,     /* Input(1 byte wheel) */
-    0xC0,           /* End Collection */
-    0xC0,           /* End Collection */
+  HID_UsagePageVendor( 0x00                      ),
+  HID_Usage          ( 0x01                      ),
+  HID_Collection     ( HID_Application           ),
+    HID_LogicalMin   ( 0                         ), /* value range: 0 - 0xFF */
+    HID_LogicalMaxS  ( 0xFF                      ),
+    HID_ReportSize   ( 8                         ), /* 8 bits */
+    HID_ReportCount  ( 64  ),
+    HID_Usage        ( 0x01                      ),
+    HID_Input        ( HID_Data | HID_Variable | HID_Absolute ),
+    HID_ReportCount  ( 64 ),
+    HID_Usage        ( 0x01                      ),
+    HID_Output       ( HID_Data | HID_Variable | HID_Absolute ),
+    HID_ReportCount  ( 1),
+    HID_Usage        ( 0x01                      ),
+    HID_Feature      ( HID_Data | HID_Variable | HID_Absolute ),
+  HID_EndCollection,
 };
 
 
@@ -47,26 +37,26 @@ uint8_t ReportDescriptor[] =
 /*!<USB Device Descriptor */
 uint8_t gu8DeviceDescriptor[] =
 {
-    LEN_DEVICE,     /* bLength */
-    DESC_DEVICE,    /* bDescriptorType */
-    0x10, 0x01,     /* bcdUSB */
-    0x00,           /* bDeviceClass */
-    0x00,           /* bDeviceSubClass */
-    0x00,           /* bDeviceProtocol */
-    8,   /* bMaxPacketSize0 */
-    /* idVendor */
-    0x0D28 & 0x00FF,
-    (0x0D28 & 0xFF00) >> 8,
-    /* idProduct */
-    0x0204 & 0x00FF,
-    (0x0204 & 0xFF00) >> 8,
-    /* bcdDevice */	
-    0x0100 & 0x00FF,
-    (0x0100 & 0xFF00) >> 8,
-    0x01,           /* iManufacture */
-    0x02,           /* iProduct */
-    0x00,           /* iSerialNumber - no serial */
-    0x01            /* bNumConfigurations */
+	LEN_DEVICE,     /* bLength */
+	DESC_DEVICE,    /* bDescriptorType */
+	0x10, 0x01,     /* bcdUSB */
+	0x00,           /* bDeviceClass */
+	0x00,           /* bDeviceSubClass */
+	0x00,           /* bDeviceProtocol */
+	8,   /* bMaxPacketSize0 */
+	/* idVendor */
+	0x0D28 & 0x00FF,
+	(0x0D28 & 0xFF00) >> 8,
+	/* idProduct */
+	0x0204 & 0x00FF,
+	(0x0204 & 0xFF00) >> 8,
+	/* bcdDevice */	
+	0x0100 & 0x00FF,
+	(0x0100 & 0xFF00) >> 8,
+	0x01,           /* iManufacture */
+	0x02,           /* iProduct */
+	0x03,           /* iSerialNumber - no serial */
+	0x01            /* bNumConfigurations */
 };
 
 /*!<USB Configure Descriptor */
@@ -75,25 +65,14 @@ uint8_t gu8ConfigDescriptor[] =
     LEN_CONFIG,     /* bLength */
     DESC_CONFIG,    /* bDescriptorType */
     /* wTotalLength */
-    LEN_CONFIG_AND_SUBORDINATE & 0x00FF,
-    (LEN_CONFIG_AND_SUBORDINATE & 0xFF00) >> 8,
+    41 & 0x00FF,                
+    (41 & 0xFF00) >> 8,
     0x01,           /* bNumInterfaces */
     0x01,           /* bConfigurationValue */
     0x00,           /* iConfiguration */
     0x80 | (0 << 6),/* bmAttributes */
     0xfa,         /* MaxPower */
-
-    /* I/F descr: HID */
-    LEN_INTERFACE,  /* bLength */
-    DESC_INTERFACE, /* bDescriptorType */
-    0x00,           /* bInterfaceNumber */
-    0x00,           /* bAlternateSetting */
-    0x01,           /* bNumEndpoints */
-    0x03,           /* bInterfaceClass */
-    0x01,           /* bInterfaceSubClass */
-    HID_MOUSE,      /* bInterfaceProtocol */
-    0x00,           /* iInterface */
-
+	
 	/* Interface, Alternate Setting 0, HID Class */
 	9,										/* bLength */                                                       \
 	4,										/* bDescriptorType */                                               \
@@ -113,8 +92,8 @@ uint8_t gu8ConfigDescriptor[] =
     0x01,           /* Number of HID class descriptors to follow. */
     0x22,   /* Dscriptor type. */
     /* Total length of report descriptor. */
-    sizeof(HID_MouseReportDescriptor) & 0x00FF,
-    (sizeof(HID_MouseReportDescriptor) & 0xFF00) >> 8,
+    sizeof(ReportDescriptor) & 0x00FF,
+    (sizeof(ReportDescriptor) & 0xFF00) >> 8,
 
     /* EP Descriptor: interrupt in. */
     7,   /* bLength */
@@ -124,7 +103,7 @@ uint8_t gu8ConfigDescriptor[] =
     /* wMaxPacketSize */
     64 & 0x00FF,
     (64 & 0xFF00) >> 8,
-    1     /* bInterval */
+    1,     /* bInterval */
 		
     /* EP Descriptor: interrupt out. */
     7,   /* bLength */
@@ -134,7 +113,7 @@ uint8_t gu8ConfigDescriptor[] =
     /* wMaxPacketSize */
     64 & 0x00FF,
     (64 & 0xFF00) >> 8,
-    1     /* bInterval */
+    1,     /* bInterval */
 };
 
 /*!<USB Language String Descriptor */
@@ -182,7 +161,7 @@ const S_USBD_INFO_T gsInfo =
     gu8DeviceDescriptor,
     gu8ConfigDescriptor,
     gpu8UsbString,
-    HID_MouseReportDescriptor
+    ReportDescriptor
 };
 
 
